c5359ea18e0a4712f8abdc748f64237efa267ef3
prostochlen <iwan.maksackov@yandex.ru>
2020-10-26 21:59:21 +0300

    add js part of project for day 3

--- js/script.js
+++ js/script.js
@@ -1,98 +1,108 @@
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict';
+document.addEventListener("DOMContentLoaded", function () {
+    "use strict";
 
-    const overlay = document.querySelector('.overlay');
-    const quiz = document.querySelector('.quiz');
-    const passTestButton = document.querySelector('.pass-test__button');
-    const form = document.querySelector('.quiz-body__form');
-    const formItems = form.querySelectorAll('fieldset');
-    const btnsNext = form.querySelectorAll('.form-button__btn-next');
-    const btnsPrev = form.querySelectorAll('.form-button__btn-prev');
-
+    const overlay = document.querySelector(".overlay");
+    const quiz = document.querySelector(".quiz");
+    const passTestButton = document.querySelector(".pass-test__button");
+    const form = document.querySelector(".quiz-body__form");
+    const formItems = form.querySelectorAll("fieldset");
+    const btnsNext = form.querySelectorAll(".form-button__btn-next");
+    const btnsPrev = form.querySelectorAll(".form-button__btn-prev");
     const answersObj = {
         step0: {
-            question: '',
-            answers: []
+            question: "",
+            answers: [],
         },
         step1: {
-            question: '',
-            answers: []
+            question: "",
+            answers: [],
         },
         step2: {
-            question: '',
-            answers: []
+            question: "",
+            answers: [],
         },
         step3: {
-            question: '',
-            answers: []
+            question: "",
+            answers: [],
         },
         step4: {
-            name: '',
-            phone: '',
-            email: '',
-            call: ''
-        }
+            name: "",
+            phone: "",
+            email: "",
+            call: "",
+        },
+    };
+
+    btnsNext.forEach((btn, btnIndex) => {
+        btn.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            formItems[btnIndex].style.display = "none";
+            formItems[btnIndex + 1].style.display = "block";
+        });
+
+        btn.disabled = true;
+    });
+
+    for (let i = 0; i < btnsPrev.length; i++) {
+        btnsPrev[i].addEventListener("click", (event) => {
+            event.preventDefault();
+
+            formItems[i + 1].style.display = "none";
+            formItems[i].style.display = "block";
+        });
     }
 
-    btnsNext.forEach((item, index) => {
-        item.addEventListener('click', event => {
-            event.preventDefault();
+    formItems.forEach((formItem, formItemIndex) => {
+        if (formItemIndex === 0) {
+            formItem.style.display = "block";
+        } else {
+            formItem.style.display = "none";
+        }
 
-            formItems[index].style.display = 'none';
-            formItems[index + 1].style.display = 'block';
-        });
+        if (formItemIndex !== formItems.length - 1) {
+            const inputs = formItem.querySelectorAll("input");
+            const itemTitle = formItem.querySelector(".form__title");
 
-        item.disabled = true;
-    });
+            answersObj[`step${formItemIndex}`].question = itemTitle.textContent;
 
-    btnsPrev.forEach((item, index) => {
-        item.addEventListener('click', event => {
-            event.preventDefault();
-
-            formItems[index + 1].style.display = 'none';
-            formItems[index].style.display = 'block';
-        });
-    });
-
-    formItems.forEach((item, index) => {
-        index === 0 ? item.style.display = 'block' : item.style.display = 'none';
-
-        if (index !== formItems.length -1 ) {
-            const itemTitle = item.querySelector('.form__title');
-            const inputs = form.querySelectorAll('input');
-
-            answersObj[`step${index}`].question = itemTitle.textContent;
-
-            inputs.forEach(input => {
-               const parent = input.parentNode;
-               input.checked = false;
-               parent.classList.remove('active-radio');
-               parent.classList.remove('active-checkbox');
+            inputs.forEach((input) => {
+                const parent = input.parentNode;
+                input.checked = false;
+                parent.classList.remove("active-radio");
+                parent.classList.remove("active-checkbox");
             });
         }
 
-        item.addEventListener('change', event => {
+        // выбор radio и checkbox
+        formItem.addEventListener("change", (event) => {
             const target = event.target;
-            const inputsChecked = item.querySelectorAll('input:checked');
+            const inputsChecked = formItem.querySelectorAll("input:checked");
 
-            if (index !== formItems.length -1 ) {
-
-                answersObj[`step${index}`].answers.length = 0;
-
-                inputsChecked.forEach((checkedItem, checkedIndex) => {
-                    answersObj[`step${index}`].answers.push(checkedItem.value);
+            if (formItemIndex !== formItems.length - 1) {
+                answersObj[`step${formItemIndex}`].answers.length = 0;
+                inputsChecked.forEach((inputChecked) => {
+                    answersObj[`step${formItemIndex}`].answers.push(inputChecked.value);
                 });
 
-                inputsChecked.length > 0 ? btnsNext[index].disabled = false : btnsNext[index].disabled = true;
+                if (inputsChecked.length > 0) {
+                    btnsNext[formItemIndex].disabled = false;
+                } else {
+                    btnsNext[formItemIndex].disabled = true;
+                }
 
-                if (target.classList.contains('form__radio')) {
-                    const radios = item.querySelectorAll('.form__radio');
+                if (target.classList.contains("form__radio")) {
+                    const radios = formItem.querySelectorAll(".form__radio");
 
-                    radios.forEach(input => {
-                        input === target ? input.parentNode.classList.add('active-radio') : input.parentNode.classList.remove('active-radio');
+                    radios.forEach((input) => {
+                        if (input === target) {
+                            input.parentNode.classList.add("active-radio");
+                        } else {
+                            input.parentNode.classList.remove("active-radio");
+                        }
                     });
-                } else if (target.classList.contains('form__input')) {
-                    target.parentNode.classList.toggle('.active-checkbox');
+                } else if (target.classList.contains("form__input")) {
+                    target.parentNode.classList.toggle("active-checkbox");
                 } else {
                     return;
                 }
@@ -101,48 +111,75 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     const sendForm = () => {
-        const lastFieldset = formItems[formItems.length - 1];
-
-        form.addEventListener('submit', event => {
+        form.addEventListener("submit", (event) => {
             event.preventDefault();
 
-            answersObj.step4.name = document.getElementById('quiz-name').value;
-            answersObj.step4.phone = document.getElementById('quiz-phone').value;
-            answersObj.step4.email = document.getElementById('quiz-email').value;
-            answersObj.step4.call = document.getElementById('quiz-call').value;
+            answersObj.step4.name = document.getElementById("quiz-name").value;
+            answersObj.step4.phone = document.getElementById("quiz-phone").value;
+            answersObj.step4.email = document.getElementById("quiz-email").value;
+            answersObj.step4.call = document.getElementById("quiz-call").value;
 
-            if (document.getElementById('quiz-policy').checked) {
-                postData(answersObj).then(res => {
-                    if (res.status === 200) {
-                        overlay.style.display = 'none';
-                        quiz.style.display = 'none';
-                        alert('Успешно отправлено');
-                    } else {
+            // for (let key in answersObj.step4) {
+            //   if (answersObj.step4[key].value === "") {
+            //     alert("Введите даные во все поля");
+            //   }
+            // }
 
-                    }
-                });
+            if (document.getElementById("quiz-policy").checked) {
+                postData(answersObj)
+                    .then((res) => res.json())
+                    .then((res) => {
+                        if (res["status"] === "ok") {
+                            overlay.style.display = "none";
+                            quiz.style.display = "none";
+                            form.reset();
+                            alert(res["message"]);
+                        } else if (res["status"] === "error") {
+                            alert(res["message"]);
+                        }
+                    });
             } else {
-                alert('Подтвердите обработку персональных данных');
+                alert("Дайте согласие на обработку персональных данных");
             }
         });
-    }
-
-    const postData = body => {
-        return fetch('./server.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        })
     };
 
-    overlay.style.display = 'none';
-    quiz.style.display = 'none';
+    const postData = (body) => {
+        return fetch("./server.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        });
+    };
 
-    passTestButton.addEventListener('click', event => {
-        overlay.style.display = 'block';
-        quiz.style.display = 'block';
+    overlay.style.display = "none";
+    quiz.style.display = "none";
+
+    passTestButton.addEventListener("click", () => {
+        formItems.forEach((formItem, formItemIndex) => {
+            if (formItemIndex === 0) {
+                formItem.style.display = "block";
+            } else {
+                formItem.style.display = "none";
+            }
+
+            const inputs = formItem.querySelectorAll("input");
+            inputs.forEach((input) => {
+                const parent = input.parentNode;
+                input.checked = false;
+                parent.classList.remove("active-radio");
+                parent.classList.remove("active-checkbox");
+            });
+        });
+
+        btnsNext.forEach((btn) => {
+            btn.disabled = true;
+        });
+
+        overlay.style.display = "block";
+        quiz.style.display = "block";
     });
 
     sendForm();
